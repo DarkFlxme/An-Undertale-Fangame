4a0cb34c9e8a08f34661ebb4564c2af381b87c9f
Emir Enç <superkillerkanali@gmail.com>
2025-09-11 18:45:05 +0300

    initial commit

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -20,83 +20,89 @@ public partial class Player : CharacterBody2D
 	public override void _PhysicsProcess(double delta)
 	{
 		float deltaF = (float)delta;
-		if (_heartSprite.Frame == 0)//red heart
+
+		switch (_heartSprite.Frame)
 		{
-			horizontalDirection = Input.GetAxis("move_left", "move_right");
-			verticalDirection = Input.GetAxis("move_up", "move_down");
-			Velocity = new Vector2(horizontalDirection, verticalDirection).Normalized() * Speed;
-		}
-		else if (_heartSprite.Frame == 1) //blue heart
-		{
-			horizontalDirection = Input.GetAxis("move_left", "move_right");
-			Velocity = new Vector2(horizontalDirection * Speed, Velocity.Y + Gravity * deltaF);
-
-			if (Input.IsActionJustPressed("move_up") && !_isJumping && IsOnFloor())
-			{
-				_isJumping = true;
-				_jumpStartY = GlobalPosition.Y;
-				Velocity = new Vector2(Velocity.X, -JumpForce);
-			}
-
-			if (_isJumping)
-			{
-				if (GlobalPosition.Y <= _jumpStartY - MaxJumpDistance)
-				{
-					_isJumping = false;
-				}
-			}
-
-			if (Input.IsActionJustReleased("move_up") && Velocity.Y < 0)
-			{
-				Velocity = new Vector2(Velocity.X, 0);
-				_isJumping = false;
-			}
-
-			if (IsOnFloor())
-			{
-				_isJumping = false;
-			}
-
-			if (IsOnCeiling())
-			{
-				_isJumping = false;
-			}
-		}
-		else if (_heartSprite.Frame == 2) //green heart
-		{
-			Nodes.DisableNode(Nodes.map1);
-			Nodes.EnableNode(Nodes.map2);
-			Position = new Vector2(583, 426);
-			Velocity = new Vector2(0, 0);
-		}
-		else if (_heartSprite.Frame == 3) // purple heart
-		{
-
-		}
-		else if (_heartSprite.Frame == 4) // yellow heart
-		{
-
-			horizontalDirection = Input.GetAxis("move_left", "move_right");
-			verticalDirection = Input.GetAxis("move_up", "move_down");
-			Velocity = new Vector2(horizontalDirection * Speed, verticalDirection * Speed);
-			if (Input.IsActionJustPressed("yellow_heart_shot"))
-			{
-				if (Nodes.BulletTimer.TimeLeft == 0)
-				{
-					var bullet = GD.Load<PackedScene>("res://scenes//Bullet.tscn").Instantiate<Bullet>();
-					bullet.Position = Position;
-					GetParent().AddChild(bullet);
-					Nodes.BulletTimer.Start(0.3);
-				}
-			}
-		}
-		else if (_heartSprite.Frame == 5)
-		{
-			horizontalDirection = Input.GetAxis("move_left", "move_right");
-			verticalDirection = Input.GetAxis("move_up", "move_down");
-			if (horizontalDirection != 0 || verticalDirection != 0)
+			case 0: // Red heart
+				horizontalDirection = Input.GetAxis("move_left", "move_right");
+				verticalDirection = Input.GetAxis("move_up", "move_down");
 				Velocity = new Vector2(horizontalDirection, verticalDirection).Normalized() * Speed;
+				break;
+
+			case 1: // Blue heart
+				if (RotationDegrees == 0)
+				{
+					horizontalDirection = Input.GetAxis("move_left", "move_right");
+					Velocity = new Vector2(horizontalDirection * Speed, Velocity.Y + Gravity * deltaF);
+
+					if (Input.IsActionJustPressed("move_up") && !_isJumping && IsOnFloor())
+					{
+						_isJumping = true;
+						_jumpStartY = GlobalPosition.Y;
+						Velocity = new Vector2(Velocity.X, -JumpForce);
+					}
+
+					if (_isJumping)
+					{
+						if (GlobalPosition.Y <= _jumpStartY - MaxJumpDistance)
+						{
+							_isJumping = false;
+						}
+					}
+
+					if (Input.IsActionJustReleased("move_up") && Velocity.Y < 0)
+					{
+						Velocity = new Vector2(Velocity.X, 0);
+						_isJumping = false;
+					}
+
+					if (IsOnFloor())
+					{
+						_isJumping = false;
+					}
+
+					if (IsOnCeiling())
+					{
+						_isJumping = false;
+					}
+				}
+				break;
+
+			case 2: // Green heart
+				Nodes.DisableNode(Nodes.map1);
+				Nodes.EnableNode(Nodes.map2);
+				Position = new Vector2(583, 426);
+				Velocity = Vector2.Zero;
+				break;
+				//complete this
+			case 3: // Purple heart
+				break;
+
+			case 4: // Yellow heart
+				horizontalDirection = Input.GetAxis("move_left", "move_right");
+				verticalDirection = Input.GetAxis("move_up", "move_down");
+				Velocity = new Vector2(horizontalDirection * Speed, verticalDirection * Speed);
+
+				if (Input.IsActionJustPressed("yellow_heart_shot"))
+				{
+					if (Nodes.BulletTimer.TimeLeft == 0)
+					{
+						var bullet = GD.Load<PackedScene>("res://scenes/Bullet.tscn").Instantiate<Bullet>();
+						bullet.Position = Position;
+						GetParent().AddChild(bullet);
+						Nodes.BulletTimer.Start(0.3);
+					}
+				}
+				break;
+
+			case 5: // orange heart
+				horizontalDirection = Input.GetAxis("move_left", "move_right");
+				verticalDirection = Input.GetAxis("move_up", "move_down");
+				if (horizontalDirection != 0 || verticalDirection != 0)
+					Velocity = new Vector2(horizontalDirection, verticalDirection).Normalized() * Speed;
+				break;
 		}
+
 		if (Input.IsActionJustPressed("0")) _heartSprite.Frame = 0;
 		else if (Input.IsActionJustPressed("1")) _heartSprite.Frame = 1;
 		else if (Input.IsActionJustPressed("2")) _heartSprite.Frame = 2;
